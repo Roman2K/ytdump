929a8e85c4ca1b567197f17b6ec2ebf99451adca
Roman Le Négrate <roman.lenegrate@gmail.com>
2019-06-02 19:02:58 +0200

    moundir: update playlists

--- playlists/moundir/dl.sh
+++ playlists/moundir/dl.sh
@@ -1,3 +1,4 @@
 exec ../../dl \
+  'https://www.6play.fr/moundir-et-les-apprentis-aventuriers-p_5848' \
   'http://www.replaytivi.fr/programme/moundir-et-les-apprentis-aventuriers' \
   --rclone_dest=drive:media/replay/moundir
--- playlists/moundir_derchance/dl.sh
+++ playlists/moundir_derchance/dl.sh
@@ -1,4 +1,5 @@
 exec ../../dl \
   'https://www.6play.fr/moundir-et-la-plage-de-la-derniere-chance-p_14151' \
   --rclone_dest=drive:media/replay/moundir_derchance \
-  --min_duration=`bc <<<"10 * 60"`
+  --min_duration=`bc <<<"10 * 60"` \
+  --no-check_empty
