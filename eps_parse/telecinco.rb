65032867e850877b2f297c30438801e3101f96a9
Roman Le Négrate <roman.lenegrate@gmail.com>
2021-05-01 21:01:44 +0200

    Telecinco: handle pagination

--- eps_parse/telecinco.rb
+++ eps_parse/telecinco.rb
@@ -12,17 +12,33 @@ class Telecinco < Parser
     cs.last == "a-la-carta" && cs.size == 3
   end
 
+  def playlist_items(uri)
+    uri = URI uri
+    page = 1
+    items = []
+    loop do
+      its = super(uri)
+      break if its.empty?
+      items = its.concat items
+      page += 1
+      uri.query = URI.encode_www_form page: page
+    end
+    items
+  end
+
   def episodes_from_doc(doc, uri)
-    doc.css("a[href^='#{uri.path}']").filter_map { |a|
+    path_prefix = uri.path.chomp "a-la-carta/"
+    doc.css("article a[href^='#{path_prefix}']").filter_map { |a|
+      title = a.text.strip
+      next if title.empty?  # could be a <img/>
       path = a["href"] or next
-      !a.text.empty? or next  # could be a <img/>
       path =~ /_(\d+)\.html$/ or next
       id = $1
       Item.new \
         id: id,
         idx: id.to_i,
-        url: uri.dup.tap { _1.path = path }.to_s,
-        title: a.text.strip
+        url: uri.dup.tap { |u| u.path = path; u.query = nil }.to_s,
+        title: title
     }.reverse
   end
 end
--- test/eps_parse/telecinco_test.rb
+++ test/eps_parse/telecinco_test.rb
@@ -12,24 +12,27 @@ class TelecincoTest < Minitest::Test
   def do_test_playlist_items
     items = Telecinco.new.playlist_items \
       "https://www.telecinco.es/la-isla-de-las-tentaciones/a-la-carta/"
-    assert_equal 9, items.size
+    assert_equal 24, items.size
 
     ep = items.fetch 0
-    assert_equal "3079395004", ep.id
-    assert_equal 3079395004, ep.idx
+    assert_equal "3083070001", ep.id
+    assert_equal 3083070001, ep.idx
     assert_equal \
-      "https://www.telecinco.es/la-isla-de-las-tentaciones/a-la-carta/programa-uno-completo-21-01-2021-hd_18_3079395004.html",
+      "https://www.telecinco.es/la-isla-de-las-tentaciones/a-la-carta/programa-dos-completo-29-01-2021-hd_18_3083070001.html",
       ep.url
     assert_nil ep.duration
     assert_equal \
-      "La isla de las tentaciones 3 | Programa 1 completo del 21-01-2021 en HD",
+      "La isla de las tentaciones 3 | Programa 2 completo del 28-01-2021 en HD",
       ep.title
 
     ep = items.fetch 1
-    assert_equal "3081495027", ep.id
+    assert_equal "3086220388", ep.id
 
     ep = items.fetch 2
-    assert_equal "3083070001", ep.id
+    assert_equal "3088845023", ep.id
+
+    ep = items.fetch -1
+    assert_equal "3115620008", ep.id
   end
 
   private def with_cache(&block)
